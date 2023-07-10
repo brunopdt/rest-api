cf364bc5b6af3a044f5d2c959418ddd60e325220
brunopdt <bruno.pduarte0@gmail.com>
2023-07-10 10:13:59 -0300

    assets e refatoração front

--- vue-rest-api/src/App.vue
+++ vue-rest-api/src/App.vue
@@ -7,10 +7,10 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
 
     <nav class="w-full flex center mx-auto p-2 h-[5vh] bg-[#4F4557]">
-      <RouterLink class="px-4 py-1 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white transition-all" to="/">Home
-      </RouterLink>
-      <RouterLink class="px-4 py-1 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white transition-all" to="/skills">
+      <RouterLink class="px-4 py-1 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white transition-all" to="/">
         Skills</RouterLink>
+      <a class="px-4 py-1 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white transition-all"
+        href="https://github.com/brunopdt/rest-api" target="_blank">Repositório</a>
     </nav>
 
   </header>
--- vue-rest-api/src/views/skills/SkillIndex.vue
+++ vue-rest-api/src/views/skills/SkillIndex.vue
@@ -25,12 +25,13 @@ onMounted(() => {
             <th scope="col" class="px-6 py-3">
               Slug
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-6 py-3 flex justify-center">
               Actions
             </th>
           </tr>
         </thead>
         <tbody>
+
           <tr v-for="skill in  skills " :key="skill.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
               {{ skill.name }}
@@ -38,7 +39,7 @@ onMounted(() => {
             <td class="px-6 py-4">
               {{ skill.slug }}
             </td>
-            <td class="py-4 flex gap-1">
+            <td class="py-4 flex gap-1 justify-center">
               <RouterLink :to="{ name: 'SkillEdit', params: { id: skill.id } }"
                 class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 transition-all rounded text-white">EDIT</RouterLink>
               <button @click="deleteSkill(skill.id)"
